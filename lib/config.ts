d1c8350ef2d577e6eb509c1df51d27c1d0f2d38e
MRK <64604054+mrk991000@users.noreply.github.com>
2026-08-29 03:19:31 +0330

    Update config.ts

--- lib/config.ts
+++ lib/config.ts
@@ -2,6 +2,7 @@
 export const GITHUB_USERNAME = "mrk991000";
 export const GITHUB_REPO = "ascend-game";
 export const APK_FILENAME = "Ascend.apk";
+
 // ────────────────────────────────────────────────────────────────────────
 
 // This pattern always points at whatever asset with this exact filename
@@ -10,3 +11,4 @@ export const APK_FILENAME = "Ascend.apk";
 export const DOWNLOAD_URL = `https://github.com/${GITHUB_USERNAME}/${GITHUB_REPO}/releases/download/latest/${APK_FILENAME}`;
 export const REPO_URL = `https://github.com/${GITHUB_USERNAME}/${GITHUB_REPO}`;
 export const RELEASES_URL = `https://github.com/${GITHUB_USERNAME}/${GITHUB_REPO}/releases`;
+export const TEST_URL = `https://ascend-bay-pi.vercel.app/`;
